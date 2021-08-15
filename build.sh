22178218ca5d016a43cb0671c37b7f4a286aae42
Siddharth Manthan <siddharth.manthan@gmail.com>
2021-08-15 21:37:16 +0530

    5.14RC4

--- build.sh
+++ build.sh
@@ -13,20 +13,18 @@ git clone --depth=1 https://github.com/msm8916-mainline/linux
 cd linux
 
 #Apply Patches and fixes
-git apply --ignore-whitespace ../patches/device-support/*.patch
-rm arch/arm64/boot/dts/qcom/apq8016-samsung-gt510wifi.dts
-git apply --ignore-whitespace ../patches/fixes/*.patch
+git apply --ignore-whitespace ../patches/*.patch
 
 #Kernel Config
 make msm8916_defconfig pmos.config
 
 #Build
 # Example : 
-#Command : ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make KERNELRELEASE="5.13-sunxi64" KDEB_PKGVERSION="5.13~rc3+sunxi64-0.1" -j64 bindeb-pkg
+#Command : ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make KERNELRELEASE="5.14-sunxi64" KDEB_PKGVERSION="5.14~rc3+sunxi64-0.1" -j64 bindeb-pkg
 #Output : linux-image-$(KERNELRELEASE)_$(KDEB_PKGVERSION)_$(ARCH).deb
 
-make KERNELRELEASE="5.13.0-rc4-postmarketos-qcom-msm8916" KDEB_PKGVERSION="5.13.0-rc4-postmarketos-qcom-msm8916-0.1" -j$(nproc) bindeb-pkg
-make KERNELRELEASE="5.13.0-rc4-postmarketos-qcom-msm8916" KDEB_PKGVERSION="5.13.0-rc4-postmarketos-qcom-msm8916-0.1" -j$(nproc) binrpm-pkg
+make KERNELRELEASE="5.14.0-rc4-postmarketos-qcom-msm8916" KDEB_PKGVERSION="5.14.0-rc4-postmarketos-qcom-msm8916-0.1" -j$(nproc) bindeb-pkg
+make KERNELRELEASE="5.14.0-rc4-postmarketos-qcom-msm8916" KDEB_PKGVERSION="5.14.0-rc4-postmarketos-qcom-msm8916-0.1" -j$(nproc) binrpm-pkg
 
 #Github artifact
 cd ..
